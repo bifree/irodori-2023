f2253fdee78b602abee16d545bb13ddc584104a3
bifree <bifree2bfree@gmail.com>
2023-04-16 01:29:01 +0900

    fix header css

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -14,15 +14,15 @@
 </head>
 <body>
 <header>
-	<div class=" header">
-		<div class="logo">
+	<div class="header">
+		<div class="header__logo">
 			<a href="<?php echo esc_url( home_url('/') ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="logo"></a>
 		</div>
 		<div class="sp-menu">
 			<span class="material-icons" id="open">menu</span>
 		</div>
 		<div class="top--menu">
-			<?php get_template_part('global_nav'); ?>
+			<?php get_template_part('template/global_nav'); ?>
 		</div>
 	</div>
 
---
+++ WordPressTheme/template/global_nav.php
@@ -0,0 +1,23 @@
+<nav>
+	<ul>
+		<li><a href="<?php echo esc_url( home_url('/about') ); ?>">HOME</a></li>
+		<li><a href="https://irodori-odori.com/about/">ABOUT</a></li>
+		<li class="sub--menu"><a class="pointer">SERVICE<img src="<?php echo get_template_directory_uri(); ?>/assets/images/path.png"></a>
+			<ul class="submenu">
+				<li><a href="https://coubic.com/irodori_odori/718251">バレエレッスン</a></li>
+				<li><a href="https://irodori-odori.com/ladies-shopping/" class="fashion">レディースファッション買い物同行</a></li>
+				<li><a href="https://irodori-odori.com/category/event_info/">イベント開催</a></li>
+			</ul>
+		</li>
+		<li class="sub--menu"><a href="https://irodori-odori.com/blog__list/">BLOG<img src="<?php echo get_template_directory_uri(); ?>/images/path.png"></a>
+			<ul class="submenu">
+				<li><a href="https://irodori-odori.com/category/news/">お知らせ</a></li>
+				<li><a href="https://irodori-odori.com/category/event_info/">イベント情報</a></li>
+				<li><a href="https://irodori-odori.com/category/activity/">活動報告</a></li>
+				</ul>
+		</li>
+		<a href="https://irodori-odori.com/contact/"><div class="contact">
+		CONTACT<i class="fas fa-paper-plane"></i>
+		</div></a>
+	</ul>
+</nav>
